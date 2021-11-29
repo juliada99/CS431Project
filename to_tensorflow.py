b79965b1492219014df7770703703072916e82a6
juliada99 <julkadamczyk99@gmail.com>
2021-11-28 19:58:01 -0800

    implemented simple analysis

--- to_tensorflow.py
+++ to_tensorflow.py
@@ -31,6 +31,7 @@ data_labels = data_features.pop('Confirmed')
 inputs = {}
 
 for name, column in data_features.items():
+  print(name, column)
   dtype = column.dtype
   if dtype == object:
     dtype = tf.string
@@ -95,8 +96,14 @@ for example in features_ds:
 # Make a dataset of (features_dict, labels) pairs:
 data_ds = tf.data.Dataset.from_tensor_slices((data_features_dict, data_labels))
 
+for features, label in data_ds:
+  print(features["Day"])
+  print(label.numpy())
+  break
+
 # Shuffle and batch the data.
 data_batches = data_ds.shuffle(len(data_labels)).batch(32)
+
 
 # Build the model on top of this:
 def data_model(preprocessing_head, inputs):
@@ -118,4 +125,51 @@ data_model = data_model(data_preprocessing, inputs)
 # Pass the dataset:
 data_model.fit(data_batches, epochs=5)
 
+
+
+"""
+Calculates global mean value of confirmed, deaths, and recovered for the given date.
+Input:
+dataset - covid dataset
+date - string date in form "MM/DD/YYYY"
+returns: mean_confirmed, mean_deaths, mean_recovered
+"""
+import datetime
+import pandas as pd
+dataset_start = datetime.datetime(2020, 1, 22)
+dataset_end = datetime.datetime(2021, 11, 13)
+
+def average_on_date(dataset, date):
+  format_str = '%m/%d/%Y' # The format
+  datetime_obj = datetime.datetime.strptime(date, format_str)
+  if datetime_obj >= dataset_start and datetime_obj <= dataset_end:
+    date = date.split("/")
+    day = int(date[1]) if date[1][0] != 0 else int(date[1][-1])
+    month = int(date[0]) if date[0][0] != 0 else int(date[0][-1])
+    year = int(date[2][-2:])
+    print(day, month, year)
+    confirmed = []
+    deaths = []
+    recovered = []
+    for features, label in dataset:
+      if features["Day"] == day and features["Month"] == month and features["Year"] == year:
+        deaths.append(features["Deaths"].numpy())
+        recovered.append(features["Recovered"].numpy())
+        confirmed.append(label.numpy())
+    confirmed = np.array(confirmed)
+    deaths = np.array(deaths)
+    recovered = np.array(recovered)
+    print(recovered)
+    means = (confirmed.mean(), deaths.mean(), recovered.mean())
+    medians = (np.median(confirmed), np.median(deaths), np.median(recovered))
+    std = (np.std(confirmed), np.std(deaths), np.std(recovered))
+    return means, medians, std
+ 
+mean, median, std = average_on_date(data_ds, "6/5/2021")
+print(mean)
+print(median)
+print(std)
+
+def average_between_dates(dataset, start_date, end_date):
+  pass
 
